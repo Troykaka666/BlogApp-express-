7e20bbf6683db3987357fc6bb2bc0b44d1cc85f6
troy9527 <bobo0917x@yahoo.com>
2019-02-12 04:53:53 +0000

    finished BlogApp by using express.js and mongoose

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer");
     
 
 mongoose.connect("mongodb://localhost/BlogApp",  {useNewUrlParser: true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+
 
 //Mongoose/ Model/ config
 var blogSchema = new mongoose.Schema({
@@ -30,6 +35,68 @@ var Blog = mongoose.model("Blog", blogSchema);
 app.get("/", function(req, res){
    res.render("blogs"); 
 });
+
+
+//Blog create route
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+})
+app.post("/blogs", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        }else{
+            res.redirect("/blogs");
+        }
+    });
+});
+
+//Show Route
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+       if(err){
+           res.redirect("/blogs");
+       } else{
+           res.render("show", {blog: foundBlog});
+       }
+    });
+});
+
+//Update Route
+app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+       if(err){
+           res.redirect("/blogs");
+       } else{
+           res.redirect("/blogs/" + req.params.id);//refresh the targeted page 
+       }
+    });
+});
+
+//Delete Route
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect("/blogs");
+        }
+    })
+});
+
+//Edit Route
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+})
+
 
 app.get("/blogs", function(req, res){
     Blog.find({}, function(err, blogs){
